796c2f3bd08e7402310771712baa68192d39d216
lihuyong <1003957188@qq.com>
2019-08-08 16:11:53 +0800

    修改时间格式化使用说明

--- src/utils/time.js
+++ src/utils/time.js
@@ -1,25 +1,22 @@
 let timeFun = {
-	getTime: function () { // 获取当前时间戳
-		return Date.parse(new Date());
-	},
-	// 时间格式化 
-	// var a = new Date(1520146144839); 
-	// var b = timeFun.format(a, "yyyy-MM-dd HH:mm:ss");
-	format : function ($thisTime,fmt) { 
-		var o = {
-			"M+": $thisTime.getMonth() + 1, //月份
-			"d+": $thisTime.getDate(), //日
-			"H+": $thisTime.getHours(), //小时
-			"m+": $thisTime.getMinutes(), //分
-			"s+": $thisTime.getSeconds(), //秒
-			"q+": Math.floor(($thisTime.getMonth() + 3) / 3), //季度
-			"S": $thisTime.getMilliseconds() //毫秒
-		};
-		if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, ($thisTime.getFullYear() + "").substr(4 - RegExp.$1.length));
-		for (var k in o)
-			if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-		return fmt;
-	}
+  getTime: function () { // 获取当前时间戳
+    return Date.parse(new Date());
+  },
+  format : function ($thisTime,fmt) { // 时间格式化
+    var o = {
+      "M+": $thisTime.getMonth() + 1, //月份
+      "d+": $thisTime.getDate(), //日
+      "H+": $thisTime.getHours(), //小时
+      "m+": $thisTime.getMinutes(), //分
+      "s+": $thisTime.getSeconds(), //秒
+      "q+": Math.floor(($thisTime.getMonth() + 3) / 3), //季度
+      "S": $thisTime.getMilliseconds() //毫秒
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, ($thisTime.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+      if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+  }
 }
 
 export {timeFun}
@@ -29,4 +26,5 @@ export {timeFun}
 // Vue.prototype.$time = timeFun
 
 // mounted 中:
-// this.$time.getTime(); 
+// this.$time.getTime();
+// this.$time.format(new Date(timestamp), "yyyy-MM-dd HH:mm:ss");
