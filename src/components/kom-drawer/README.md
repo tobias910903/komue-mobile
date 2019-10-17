e544f21209c52654f7184ea2a9d784bb85f60ec9
komorebi <1003957188@qq.com>
2019-10-17 20:02:52 +0800

    更新抽屉组件、增加展开方向

--- src/components/kom-drawer/README.md
+++ src/components/kom-drawer/README.md
@@ -1,32 +1,34 @@
 ```
 <template>
-  <div>
-    <!-- 点击按钮 显示侧滑菜单 -->
-    <button @click="setDrawer(true)">显示</button>
-    <button @click="setDrawer(false)">隐藏</button>
-
-    <!-- 侧滑菜单 -->
-    <kom-drawer :drawerShow="drawerShow" v-on:hideDrawer="setDrawer(false)"></kom-drawer>
-  </div>
+    <div>
+        <!-- 点击按钮 显示侧滑菜单 -->
+        <button @click="setDrawer(true)">显示</button>
+        <button @click="setDrawer(false)">隐藏</button>
+        
+        <!-- 侧滑菜单 -->
+        <kom-drawer direction="top" :drawerShow="drawerShow" v-on:hideDrawer="setDrawer(false)"></kom-drawer>
+    </div>
 </template>
 
 <script>
-  import KomDrawer from '@/components/kom-drawer'
-
-  export default {
-    data() {
-      return {
-        drawerShow: false // 默认值
-      }
-    },
-    components: {
-      KomDrawer
-    },
-    methods: {
-      setDrawer(val){
-        this.drawerShow = val;
-      }
+    import KomDrawer from '@/components/kom-drawer'
+    
+    export default {
+        data() {
+            return {
+                drawerShow: false // 默认值
+            }
+        },
+        components: {
+            KomDrawer
+        },
+        methods: {
+            setDrawer(val) {
+                this.drawerShow = val;
+            }
+        }
     }
-  }
 </script>
 ```
+
+> 抽屉
--- src/components/kom-drawer/index.vue
+++ src/components/kom-drawer/index.vue
@@ -1,73 +1,136 @@
 <template>
-  <div class="kom-drawer">
-    <transition name="fold">
-      <div class="content" v-show="drawerShow">
-        <slot></slot>
-      </div>
-    </transition>
-
-    <transition name="fade">
-      <div class="mask" @click.stop.prevent="hide" v-show="drawerShow"></div>
-    </transition>
-  </div>
+    <div class="kom-drawer">
+        <transition :name="fold">
+            <div class="content" :class="fold" v-show="drawerShow">
+                <slot></slot>
+            </div>
+        </transition>
+        
+        <transition name="fade">
+            <div class="mask" @click.stop.prevent="hide" v-show="drawerShow"></div>
+        </transition>
+    </div>
 </template>
 
 <script>
-  export default {
-    name: "komDrawer",
-    props: {
-      drawerShow: {
-        type: Boolean,
-        default: false
-      }
-    },
-    data() {
-      return {
-        data: []
-      }
-    },
-    methods: {
-      hide() {
-        this.$emit('hideDrawer', false);
-      }
+    export default {
+        name: "KomDrawer",
+        data() {
+            return {
+                fold: "fold-right" // 弹窗层的方向 top right bottom left
+            }
+        },
+        props: {
+            drawerShow: {
+                type: Boolean,
+                default: false
+            },
+            direction: {
+                type: String,
+                default: 'right'
+            }
+        },
+        methods: {
+            hide() {
+                this.$emit('hideDrawer', false);
+            }
+        },
+        mounted() {
+            console.log(this.direction);
+            this.fold = "fold-" + this.direction;
+        }
     }
-  }
 </script>
 
 <style scoped lang="less">
-.kom-drawer{
-  .content{
-    position: fixed;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    z-index: 50;
-    width: 70%;
-    background-color: #ffffff;
-    &.fold-enter-active, &.fold-leave-active{
-      transition: all .3s;
+    .kom-drawer {
+        .content {
+            position: fixed;
+            z-index: 50;
+            background-color: #ffffff;
+            
+            &.fold-top {
+                top: 0;
+                left: 0;
+                right: 0;
+                height: 50%;
+            }
+            
+            &.fold-right {
+                top: 0;
+                right: 0;
+                bottom: 0;
+                width: 70%;
+            }
+            
+            &.fold-bottom {
+                right: 0;
+                left: 0;
+                bottom: 0;
+                height: 50%;
+            }
+            
+            &.fold-left {
+                top: 0;
+                left: 0;
+                bottom: 0;
+                width: 70%;
+            }
+            
+            &.fold-top-enter-active, &.fold-top-leave-active {
+                transition: all .3s;
+            }
+            
+            &.fold-top-enter, &.fold-top-leave-to {
+                top: -50%;
+                opacity: 0;
+            }
+            
+            &.fold-right-enter-active, &.fold-right-leave-active {
+                transition: all .3s;
+            }
+            
+            &.fold-right-enter, &.fold-right-leave-to {
+                right: -70%;
+                opacity: 0;
+            }
+            
+            &.fold-bottom-enter-active, &.fold-bottom-leave-active {
+                transition: all .3s;
+            }
+            
+            &.fold-bottom-enter, &.fold-bottom-leave-to {
+                bottom: -50%;
+                opacity: 0;
+            }
+            
+            &.fold-left-enter-active, &.fold-left-leave-active {
+                transition: all .3s;
+            }
+            
+            &.fold-left-enter, &.fold-left-leave-to {
+                left: -70%;
+                opacity: 0;
+            }
+        }
+        
+        .mask {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            z-index: 40;
+            background: rgba(7, 17, 27, 0.5);
+            opacity: 1;
+            
+            &.fade-enter-active, &.fade-leave-active {
+                transition: all 0.3s ease;
+            }
+            
+            &.fade-enter, &.fade-leave-to {
+                opacity: 0;
+            }
+        }
     }
-    &.fold-enter,&.fold-leave-to{
-      right: -70%;
-      opacity: 0;
-    }
-  }
-
-  .mask{
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    z-index: 40;
-    background: rgba(7, 17, 27, 0.5);
-    opacity: 1;
-    &.fade-enter-active, &.fade-leave-active{
-      transition: all 0.3s ease;
-    }
-    &.fade-enter, &.fade-leave-to{
-      opacity: 0;
-    }
-  }
-}
 </style>
