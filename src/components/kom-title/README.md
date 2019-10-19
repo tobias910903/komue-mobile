189a97c11dc22ecc49f5eedcfb9fa0f16e700a53
lihuyong <admin@lihuyong.com>
2019-10-19 23:25:54 +0800

    标题组件

---
+++ src/components/kom-title/README.md
@@ -0,0 +1,19 @@
+```
+<template>
+    <div>
+        <kom-title title="快捷导航"></kom-title>
+    </div>
+</template>
+
+<script>
+    import KomTitle from '@/components/kom-title'
+
+    export default {
+        components: {
+            KomTitle
+        }
+    }
+</script>
+```
+
+> 标题
---
+++ src/components/kom-title/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="kom-section-title">
+        {{title}}
+    </div>
+</template>
+<script>
+    export default {
+        props: {
+            title: {
+                type: String,
+                default: ""
+            }
+        }
+    };
+</script>
+<style lang="less" scoped>
+    .kom-section-title {
+        margin: 15px 0 10px;
+        padding: 0 20px;
+        font-size: 14px;
+        color: #787878;
+    }
+</style>
