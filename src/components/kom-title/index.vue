7aa0ad65c2cef2508612ab64738bf540dc29c99c
lihuyong <admin@lihuyong.com>
2019-10-27 00:58:13 +0800

    更新样式

--- src/components/kom-title/index.vue
+++ src/components/kom-title/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="kom-section-title">
-        <h5>{{title}}</h5>
+        <h3>{{title}}</h3>
     </div>
 </template>
 <script>
@@ -15,14 +15,13 @@
 </script>
 <style lang="less" scoped>
     .kom-section-title {
-        margin: 12px 0 8px;
-        padding: 0 10px;
-        font-size: 12px;
-        color: #aaaaaa;
-        h5{
+        margin: 12px 0 6px;
+        padding: 0 12px;
+        color: #777777;
+        h3{
             display: inline-block;
-            font-weight: 400;
-            letter-spacing: 1px;
+            font-weight: 100;
+            font-size: 14px;
         }
     }
 </style>
