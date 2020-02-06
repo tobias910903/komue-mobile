928ecf0f1f99d0bdc4c73e5110154d4873fda9fa
komorebi <1003957188@qq.com>
2020-02-06 14:40:17 +0800

    弹框组件提取

---
+++ src/components/messagebox/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Alert from './alert'
+import Confirm from './confirm'
+import Toast from './toast'
+import Loading from './loading'
+
+import wcThen from './directives/wc-then'
+import wcCatch from './directives/wc-catch'
+import wcMask from './directives/wc-mask'
+import wcPopup from './directives/wc-popup'
+
+import './transition.css'
+
+export default {
+    install(Vue) {
+        Vue.directive('wcThen', wcThen);
+        Vue.directive('wcCatch', wcCatch);
+        Vue.directive('wcMask', wcMask);
+        Vue.directive('wcPopup', wcPopup);
+    }
+}
+
+export {
+    Alert,
+    Confirm,
+    Toast,
+    Loading
+}
