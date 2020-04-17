fd32a07d628d344d69be54d937e6f10e9a7be738
lihuyong <admin@lihuyong.com>
2020-04-17 14:44:55 +0800

    update

--- src/views/welcome.vue
+++ src/views/welcome.vue
@@ -1,13 +1,18 @@
 <template>
-    <kom-icon icon="kicondingwei" @click="testClick" color="#ff0000" size="50px"></kom-icon>
+    <div>欢迎页，判断页面是否登录，或者其他情况来进行页面跳转</div>
 </template>
 
 <script>
     export default {
-        methods: {
-            testClick() {
-                alert(213)
-            }
+        data() {
+            return {}
+        },
+        mounted() {
+            console.log(this);
         }
     }
 </script>
+
+<style lang="less" scoped>
+
+</style>
